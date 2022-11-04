ab526fb46e8133774a528d37fc6542d202438df9
TetyanaVynnyk <kharchenko1988@bigmir.net>
2022-11-04 18:50:18 +0200

    Tasks 1-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const widgetTextEl = document.querySelector('.color');
+
+function changeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  widgetTextEl.textContent = getRandomHexColor();
+}
+
+btnChangeColor.addEventListener('click', changeBodyColor);
